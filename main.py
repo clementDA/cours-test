f9c5b88d58312738bdba3bec61110de61bd93d6e
Clement <none>
2025-06-10 17:16:41 +0200

    Premiere version

---
+++ main.py
@@ -0,0 +1,18 @@
+import function
+
+
+
+
+
+
+def main():
+    function.greeting()
+    userInput = input("Veuillez entrer une chaine: ")
+    function.palindrome(userInput)
+
+
+    
+    
+
+if __name__ == "__main__":
+    main()
---
+++ test.py
@@ -0,0 +1,8 @@
+import unittest
+from function import is_palindrome,greeting
+
+
+
+class Test
+
+    
