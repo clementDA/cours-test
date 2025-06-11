ee42532a4e767e4de5e748e295e23d0e81cf90d8
Clement <none>
2025-06-11 10:48:00 +0200

    New update

--- test/test_palindrome.py
+++ test/test_palindrome.py
@@ -1,18 +1,10 @@
 import unittest
-from function import palindrom
-
-
+from src.function import palindrome
 
 class TestPalindrome(unittest.TestCase):
 
     def test_palindrome_simple(self):
-        self.assertEqual(palindrom("radar"), "Bien dit!")
-
-    
-
+        self.assertEqual(palindrome("radar"), "Bien dit!")
 
 if __name__ == '__main__':
     unittest.main()
-
-
-    
