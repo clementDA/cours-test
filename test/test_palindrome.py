992c698485d5e3be83c89520a6775f488920d4b8
Clement <none>
2025-06-11 17:21:03 +0200

    New update

--- test/test_palindrome.py
+++ test/test_palindrome.py
@@ -1,10 +1,25 @@
 import unittest
-from src.function import palindrome
+from src.ohce import OHCE
 
 class TestPalindrome(unittest.TestCase):
 
-    def test_palindrome_simple(self):
-        self.assertEqual(palindrome("radar"), "Bien dit!")
+    def setUp(self):
+        self.palind = OHCE()
+
+    def test_palindrome_vide(self):
+        self.assertIn("Bien dit!", self.palind.palindrome(""))
+
+    def test_palindrome_palindrome(self):
+        self.assertIn("Bien dit!", self.palind.palindrome("radar"))
+
+    def test_palindrome_non_palindrome(self):
+        self.assertIn("nipal", self.palind.palindrome("lapin"))
+
+    def test_bonjour(self):
+        self.assertIn("Bonjour", self.palind.palindrome(""))
+
+    def test_aurevoir(self):
+        self.assertIn("Au revoir", self.palind.palindrome(""))
 
 if __name__ == '__main__':
     unittest.main()
